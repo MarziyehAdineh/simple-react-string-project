e37e88fdf2324817005ebcbb49a92d6d835d3967
m <MarziyehAdineh1.gmail.com>
2021-09-14 19:36:07 +0430

    Initial commit

---
+++ src/MyComponent.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import "./styleComponent.css";
+export default class MyComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { str: ""};
+  }
+  handleChange = (e) => {
+    this.setState({ str: e.target.value });
+  };
+  
+  render() {
+    return (
+      <div className="mainContainer">
+        <input
+          className="inputStyle"
+          style={{ borderwidth: 1, margin: 10 }}
+          placeholder="متن"
+          onChange={this.handleChange}
+        />
+        <input
+          className="inputStyle"
+          style={{ borderwidth: 1, margin: 10 }}
+          placeholder="معکوس متن"
+          value={this.state.str.split("").reverse().join("")}
+        />
+        <label className="outStyle">طول رشته: {this.state.str.length}</label>
+      </div>
+    );
+  }
+}
